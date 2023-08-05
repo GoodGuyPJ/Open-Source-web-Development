897dbf3c4f334bf8460aae71424964b2846e1bc9
Pushkar Kumar <workwithgoodguypj@gmail.com>
2023-08-05 21:19:24 +0530

    commit

---
+++ week1/script.js
@@ -0,0 +1,17 @@
+var name = "Mr Jon"
+console.log(name)
+
+var users = ["Jonny", "damme", "sammer", "namin"];
+
+for(var i =0; i<4; i++){
+    console.log(users[i]);
+}
+
+var user = [
+    {name: "pewds", age: 22},
+    {name: "ram", age: 20}
+];
+
+for(var i=0; i<user.length; i++){
+    console.log(user[i].name + " age is " + user[i].age)
+}
