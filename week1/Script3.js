0e00177f0fb197a49bd6de0bbbd0a75d147ca4b1
Pushkar Kumar <workwithgoodguypj@gmail.com>
2023-08-10 20:32:05 +0530

    commit

---
+++ week1/Script3.js
@@ -0,0 +1,18 @@
+
+function pirntpartialRow(){
+    
+}
+
+function PrintsinglefullRow() {
+    var str = "";
+    for(var i=0; i<navigator; i++){
+        str = str+ "*"
+    }
+    console.log(str);
+}
+
+function StarPattern(num){
+
+}
+
+StarPattern(5);
--- week1/script2.js
+++ week1/script2.js
@@ -9,4 +9,17 @@ Native Js vs APis
 */
 // javascript can run on:- Browser, Data CustomElementRegistry, Flight TouchEvent, Moblie Phones
 
-//Java
+//Javascript is loosely typed 
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/GoodGuyPJ/Open-Source-web-Development.git
+// git push -u origin main
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/GoodGuyPJ/Open-Source-web-Development.git
+// git branch -M main
+// git push -u origin main
